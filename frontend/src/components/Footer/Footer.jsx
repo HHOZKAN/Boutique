78d4caace2d5dcb4ad2bd9f20478b6d86a05b28d
HHOZKAN <ozkn.haci@gmail.com>
2024-03-05 07:23:53 +0100

    last changes

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai";
 import { BsFacebook } from "react-icons/bs";
 import { RiInstagramFill } from "react-icons/ri";
+import "./footer.css";
 
 export const Footer = () => {
     return (
